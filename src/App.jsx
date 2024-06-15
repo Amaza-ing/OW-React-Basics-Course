2b174a09755352ff5fd63a6662bfbc2f56041feb
Adrián Maza <adri_mv1@hotmail.com>
2024-06-15 11:42:06 +0200

    04-events

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,23 @@ import ButtonComponent from "./components/ButtonComponent";
 import HeaderComponent from "./components/HeaderComponent";
 
 function App() {
+  const sayHello = () => {
+    console.log("Hello! 😄");
+  };
+
+  const handleChange = (e) => {
+    console.log(e.target.value);
+  };
+
   return (
     <>
       <HeaderComponent></HeaderComponent>
       <main className="main-content">
-        <h1>Hola a todos!</h1>
+        <h1 onClick={sayHello}>Saludos!</h1>
+        <input type="text" onChange={handleChange} />
+
+        <br />
+        <br />
         <ButtonComponent></ButtonComponent>
       </main>
     </>
