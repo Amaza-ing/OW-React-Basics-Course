362d67daeb4289940270c68b59d5f4ed5a5c20ee
Adrián Maza <adri_mv1@hotmail.com>
2024-06-17 11:29:02 +0200

    08-conditional-rendering

--- src/App.jsx
+++ src/App.jsx
@@ -19,13 +19,24 @@ function App() {
     setUser(userInfo);
   };
 
+  const condition = false;
+
   return (
     <>
       <HeaderComponent greetings={greetings} links={links}></HeaderComponent>
 
       <main className="main-content">
-        <h2>Saludos {user.username}!</h2>
+        {user.username && <h2>Saludos {user.username}!</h2>}
         <Login handleLogin={login}></Login>
+
+        {condition && <h2>La condición se cumple</h2>}
+        {!condition && <h2>La condición No se cumple</h2>}
+
+        {condition ? (
+          <h2>La condición se cumple</h2>
+        ) : (
+          <h2>La condición No se cumple</h2>
+        )}
       </main>
     </>
   );
