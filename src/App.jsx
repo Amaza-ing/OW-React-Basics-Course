b01447304de8dac455083adb5d5d269eb739c450
Adrián Maza <adri_mv1@hotmail.com>
2024-06-17 11:09:47 +0200

    07-props-child-to-parent

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import HeaderComponent from "./components/HeaderComponent";
-import ButtonComponent from "./components/ButtonComponent";
+import Login from "./components/Login";
 import { useState } from "react";
 
 function App() {
@@ -12,13 +12,20 @@ function App() {
     contact: "Contact Us",
   };
 
+  const [user, setUser] = useState({});
+
+  const login = (userInfo) => {
+    console.log(userInfo);
+    setUser(userInfo);
+  };
+
   return (
     <>
       <HeaderComponent greetings={greetings} links={links}></HeaderComponent>
 
       <main className="main-content">
-        <h2>Saludos!</h2>
-        <ButtonComponent text={"Click Me"}></ButtonComponent>
+        <h2>Saludos {user.username}!</h2>
+        <Login handleLogin={login}></Login>
       </main>
     </>
   );
