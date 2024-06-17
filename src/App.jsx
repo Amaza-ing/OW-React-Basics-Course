d7e7c917938903a04c4740a0e0124ace5a7972fe
Adrián Maza <adri_mv1@hotmail.com>
2024-06-17 12:46:28 +0200

    11-forms

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import "./App.css";
 import HeaderComponent from "./components/HeaderComponent";
 import Login from "./components/Login";
-import MovieList from "./components/MovieList";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 function App() {
   const [greetings, setGreetings] = useState("Bienvenidos a mi Web!");
@@ -15,20 +14,10 @@ function App() {
 
   const [user, setUser] = useState({});
 
-  const [showMovies, setShowMovies] = useState(true);
-
   const login = (userInfo) => {
     console.log(userInfo);
     setUser(userInfo);
   };
-
-  // useEffect(() => {
-  //   console.log("Ejecución con cada renderización del componente raíz");
-  // });
-
-  useEffect(() => {
-    console.log("Ejecución con cada cambio de la variable reactiva user");
-  }, [user]);
 
   return (
     <>
@@ -37,11 +26,6 @@ function App() {
       <main className="main-content">
         {user.username && <h2>Saludos {user.username}!</h2>}
         <Login handleLogin={login}></Login>
-
-        <button onClick={() => setShowMovies(!showMovies)}>
-          Toggle Movies
-        </button>
-        {showMovies && <MovieList></MovieList>}
       </main>
     </>
   );
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,17 +1,45 @@
 function Login(props) {
   const user = {
-    username: "Adrián",
-    email: "adrian@email.com",
+    username: "",
+    email: "",
   };
 
-  const handleClick = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
     props.handleLogin(user);
+  };
+
+  // const setUsername = (e) => {
+  //   user.username = e.target.value;
+  // };
+
+  const setUserField = (e, field) => {
+    user[field] = e.target.value;
   };
 
   return (
     <section>
       <h2>Login section</h2>
-      <button onClick={handleClick}>Login</button>
+
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            onChange={(e) => setUserField(e, "username")}
+          />
+        </fieldset>
+        <fieldset>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="text"
+            id="email"
+            onChange={(e) => setUserField(e, "email")}
+          />
+        </fieldset>
+        <button>Login</button>
+      </form>
     </section>
   );
 }
